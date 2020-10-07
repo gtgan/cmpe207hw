34077b37ae8983ec384b98dd7cdfa94379984722
ksbains <k1bains@ucsd.edu>
2020-10-06 21:26:35 -0700

    need to add broadcasting,nicknames, append message, {user} is typing, show who is online, private message

--- hw3JS/index.ts
+++ hw3JS/index.ts
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
