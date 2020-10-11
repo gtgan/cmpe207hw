e7bfdfe277e594f2d04e3b6e2293a4d6ca884beb
ksbains <k1bains@ucsd.edu>
2020-10-11 16:33:09 -0700

    started from source code, got the online, typing feature to work.

--- hw3JS/index.js
+++ hw3JS/index.js
@@ -1,16 +1,65 @@
-var app = require('express')();
+//  these are all of the package dependendancies. 
+const express = require('express')
+const app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+
+//this will host a public folder as static on the server, this for the script. 
+app.use(express.static('public'))
+
+//this will route the user to the file at the defualt route. 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+/*The getUsers method will get all of the clients that are connected.
+With this object will get all of the socket information. 
+Lastly will then map that to an array of users and return the array */
+const getUsers = () => {
+  let clients = io.sockets.clients().connected;
+  let sockets = Object.values(clients);
+  let users = sockets.map(s => s.user);
+  return users
+}
+
+//this will emit the users, the client can listen to this event to know all of the connected users
+const emitUsers = () => {
+  io.emit("users", getUsers());
+};
+
+
+//this is the fucntion that runs as socket is connected. 
 io.on('connection', function(socket){
+  let username = "username"
+  // the server will listen for a new user. 
+  socket.on("new_user", user => {
+    console.log("The user connected is: " + user)
+    let username = user
+    socket.user = user;
+    emitUsers()
+  })
+
+  socket.on('typer', username =>{
+    let clients = io.sockets.clients().connected;
+    let sockets = Object.values(clients);
+    sockets.forEach(socket => {
+      if(socket.user != username)
+        socket.emit("nosy", username);
+    });
+  });
+
+
+
+  socket.on("disconnect", function(){
+    console.log("the user: " + username + " disconnected")
+    emitUsers()
+  })
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
 });
 
 http.listen(port, function(){
--- hw3JS/public/script.js
+++ hw3JS/public/script.js
@@ -1,8 +1,81 @@
-//let userName = prompt("A user has connected! what is your name?")
+// this is the script that is running on the client end. 
+
+/* This will render the table body.
+It does so by mapping the each visitor in the state array to a tabel row with each of it's properties.*/
+
+username = prompt("Hey! welcome to the chat server! What is your username?")
+$(function () {
+    var socket = io();
+    //the client will emit the new user event with the visitor info to the server
+    socket.emit("new_user", username);
+
+    //   THIS IS THE TYPING INDICATOR CODE
+    var textarea = $('#m');
+    var typingStatus = $('#typer');
+    var lastTypedTime = new Date(0); // it's 01/01/1970, actually some time in the past
+    var typingDelayMillis = 5000; // how long user can "think about his spelling" before we show "No one is typing -blank space." message
+
+    function refreshTypingStatus() {
+
+      if (!textarea.is(':focus') ||
+          textarea.val() == '' ||
+          new Date().getTime() - lastTypedTime.getTime() > typingDelayMillis) {
+        // console.log("No one is typing")
+      } else {
+        console.log(username + " is typing")
+        socket.emit('typer', username);
+      }
+    }
+
+    function updateLastTypedTime() {
+      lastTypedTime = new Date();
+    }
+
+    setInterval(refreshTypingStatus, 100);
+    textarea.keypress(updateLastTypedTime);
 
 
+    socket.on("nosy", user => {
+      userHTML = "<p>"+user+" is typing</p>"
+      userString = user+" is typing"
+      var children = $('#typer').children();
 
-//Need to emit a socket event that will let the other users know another user has entered th chat
-//alert(userName + ' has enterd that chat')
+      if($('#typer').children().length > 0){
+        children.each(function () {
+          if(!$(this).text().includes(userString)){
+            $('#typer').append(userHTML)
+          }
 
+        });
+      } else {
+        $('#typer').append(userHTML)
+      }
+    
+    })
+    // end of the indicator code
 
+    //the client will listen for users added or leaving
+    socket.on("users", users =>{
+        //Need to update the online users view. 
+        $('#daBod').empty();
+        users.forEach(user => {
+            $('#daBod').append("<tr><td>"+ user+"</td><td>:)</td></tr>");
+        });
+    })
+
+    // this function will call the 'chat message' event with the message when the user submits the form. 
+    $('form').submit(function(){
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      $('#typer').empty()
+      return false;
+    });
+
+  
+    
+    // the client is listening for the 'chat message' event to be emiited. once it is, it will append the message as a <li>
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+      window.scrollTo(0, document.body.scrollHeight);
+    });   
+  });
